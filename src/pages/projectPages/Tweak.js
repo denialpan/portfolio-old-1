49e2aad7642935c975cd3f5987d8a5c426c1481d
denial <danpan1232@gmail.com>
2023-10-18 02:29:40 -0400

    rewrite project page, account for page zoom

--- src/pages/projectPages/Tweak.js
+++ src/pages/projectPages/Tweak.js
@@ -23,14 +23,30 @@ function Tweak(props) {
 
             <p> Up until iOS 16 was released in September 2022, there was no native way to cycle through multiple wallpapers on an iPhone, a feature that Android and Windows had decades ago. This was a frustration for me, so I went to create <b>DoABarrelWall</b>, a way to natively add this feature to set multiple homescreen and lockscreen wallpapers. </p>
 
-            <figure className='center-all-of-this'>
+            {/* <figure className='center-all-of-this'>
                 <motion.img className="tweak-images" width="200" src={doabarrelwall} alt="bobby"></motion.img>
                 <motion.img className="tweak-images" width="200" src={images} alt="bobby"></motion.img>
                 <figcaption className='image-caption'>
                 <i>Fig 1: Initial tweak landing page, with various settings for operation and performance. Fig 2: Page to add, remove, and save images to act as wallpapers to cycle through.</i> 
                 </figcaption>
                 
-            </figure>
+            </figure> */}
+
+            <p className='center-figures'>
+                <figure>
+                    <embed className="project-images" type='image/jpg' src={doabarrelwall} width="100%" ></embed>
+                    <figcaption>
+                        <i>Initial tweak landing page, with various settings for operation and performance.</i>
+                    </figcaption>
+                </figure>
+
+                <figure>
+                    <embed className="project-images" type='image/jpg' src={images} width="100%" ></embed>
+                    <figcaption>
+                        <i>Page to add, remove, and save images to act as wallpapers to cycle through.</i>
+                    </figcaption>
+                </figure>
+            </p>
 
             <p> The tweak was well received by the <u><Link className="embedded-link"to="https://www.reddit.com/r/jailbreak/comments/lkafdh/free_release_doabarrelwall_change_lockscreen_and/" target="_blank" >community</Link></u>. Upon release, I got positive feedback, many downloads, and requests for new features. As time went on, I not only implemented a majority these requests, but on a deeper level, optimized the tweak to utilize less memory and load backgrounds faster. I also learned a lot from two other people well-known in the community, and with their contribution, I learned new methods to collaborative and joint work to implement pre-existing libraries to my current project.
              </p>
@@ -38,6 +54,12 @@ function Tweak(props) {
             <p> Eventually, this tweak became end-of-life, due to Apple adding the feature to cycle through an album of pictures officially in iOS 16, so support for this tweak ended around iOS 15. However, I enjoyed the creation process immensely; this project was my first major project into creating something that others could use and freely experience. In the past, all of my projects have consisted to just run locally and were small tests that I would fool around with myself, so this project is a big accomplishment for me and and I'm extremely proud of what I have been able to create and the people I worked with. </p>
             
             <p> You can view this project on <u><Link className="embedded-link"to="https://github.com/denialpan/DoABarrelWall" target="_blank" >Github</Link></u> and my release post on <u><Link className="embedded-link"to="https://www.reddit.com/r/jailbreak/comments/lkafdh/free_release_doabarrelwall_change_lockscreen_and/" target="_blank" >Reddit</Link></u>!</p>
+
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
             
         </motion.div>
 
