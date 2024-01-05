3eb573783877e8fa797538332bf8077c375ce7ac
Daniel <danpan1232@gmail.com>
2024-01-05 05:12:46 -0500

    Update resume links and title tab

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { motion } from "framer-motion"
 import './Home.scss'
-  
+
 function Home() {
 
     const text1 = 'Hello! This is what would have been the landing page for my first portfolio, but after spending almost half a year on this, I realized I never liked it in the beginning. This portfolio has not been published before, as it was never finished in my eyes.';
@@ -13,7 +13,7 @@ function Home() {
 
     return (
 
-        <motion.div className="home-container" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+        <motion.div className="home-container" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
 
             <h1 className="home-name">  Daniel Pan </h1>
 
@@ -21,13 +21,13 @@ function Home() {
             <p className='home-description'> {text2} </p>
             <p className='home-description'> {text3} </p>
 
-            <p className='home-description center-all-of-this'> 
-      
+            <p className='home-description center-all-of-this'>
+
                 <span><Link to="/about">About</Link></span>
                 <span><Link to="/projects">Projects</Link></span>
                 <span><Link to="/blog">Blog</Link></span>
-                <span><Link to="https://raw.githubusercontent.com/denialpan/portfolio/49e2aad7642935c975cd3f5987d8a5c426c1481d/src/Daniel%20Pan%20-%20Resume.pdf" target="_blank" >Resume</Link></span>              
-                
+                <span><Link to="https://denialpan.github.io/portfolio/" target="_blank" >Resume</Link></span>
+
             </p>
 
         </motion.div>
